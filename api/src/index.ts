72df7d2d11051c47e573b429a9371b8428ffd2c1
Felipe Dornelas <m@felipedornelas.com>
2024-04-20 15:42:05 -0300

    Migrate TypeScript project from commonjs to ES modules

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,6 +6,7 @@ import { loadSchemaSync } from '@graphql-tools/load'
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 
 
+const __dirname = import.meta.dirname;
 const schema = loadSchemaSync(join(__dirname, '..', 'schema.graphql'), {
     loaders: [new GraphQLFileLoader()]
 })
