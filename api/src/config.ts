57b5ef9b0e8a79b9bfd3d474c65e71075c85a2bd
Felipe Dornelas <m@felipedornelas.com>
2024-04-20 17:21:02 -0300

    Validate configuration when its loaded from the environment

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,2 +1,15 @@
 
-export const GRAPHQL_API_PORT = parseInt(process.env.API_GRAPHQL_API_PORT || '4000')
+const requireString = (key: string): string => {
+    const value = process.env[key]
+    if (value) {
+        return value
+    }
+    throw new Error(`Could not load ${key} from the environment, or it has an empty value`)
+}
+
+const requireInt = (key: string): number => {
+    const value = requireString(key)
+    return parseInt(value)
+}
+
+export const GRAPHQL_API_PORT = requireInt('API_GRAPHQL_API_PORT')
--- client-ts/src/config.ts
+++ client-ts/src/config.ts
@@ -1,2 +1,10 @@
 
-export const GRAPHQL_API_ENDPOINT = process.env.CLIENT_GRAPHQL_API_ENDPOINT || 'http://localhost:4000/graphql'
+const requireString = (key: string): string => {
+    const value = process.env[key]
+    if (value) {
+        return value
+    }
+    throw new Error(`Could not load ${key} from the environment, or it has an empty value`)
+}
+
+export const GRAPHQL_API_ENDPOINT = requireString('CLIENT_GRAPHQL_API_ENDPOINT')
